b6ff974c5361fb0287a3e4d575e5331917592a3e
Isaac <tux314159@gmail.com>
2022-05-19 17:40:58 +0800

    black

--- get-comment.py
+++ get-comment.py
@@ -7,14 +7,14 @@ import parsedesktop
 
 # Command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('progname')
+parser.add_argument("progname")
 args = parser.parse_args()
 
-application_dir = '/usr/share/applications/'
-desktop_files = glob.glob(application_dir + '*.desktop')
+application_dir = "/usr/share/applications/"
+desktop_files = glob.glob(application_dir + "*.desktop")
 progs = parsedesktop.parsedesktop(desktop_files)
 for cprog in progs:
     if cprog[0].strip() == args.progname.strip():
-        print('\033[1m\033[34m' + args.progname.strip() + '\033[0m')
+        print("\033[1m\033[34m" + args.progname.strip() + "\033[0m")
         print(cprog[1].strip())
         sys.exit(0)
--- parsedesktop.py
+++ parsedesktop.py
@@ -1,45 +1,46 @@
 def parsedesktop(files):
     progs = []
     for cfilepath in files:
-        with open(cfilepath, 'r') as cfile:
+        with open(cfilepath, "r") as cfile:
             # Check if this is a valid Desktop file...
             cline = cfile.readline().strip()
-            while cline[0] == '#' or cline[0] == '':
+            while cline[0] == "#" or cline[0] == "":
                 cline = cfile.readline().strip()
-            if cline != '[Desktop Entry]':
+            if cline != "[Desktop Entry]":
                 continue  # if not, don't bother parsing
 
             # Look for keys
-            cline = '_'
-            cname = ''
+            cline = "_"
+            cname = ""
             namep = False
-            ccomment = ''
+            ccomment = ""
             commentp = False
-            cexec = ''
+            cexec = ""
             execp = False
-            cterminal = ''
+            cterminal = ""
             terminalp = False
             while cline:
                 cline = cfile.readline()
-                if cline[:5] == 'Name=' and not namep:
+                if cline[:5] == "Name=" and not namep:
                     cname = cline[5:]
                     namep = True
-                if cline[:8] == 'Comment=' and not commentp:
+                if cline[:8] == "Comment=" and not commentp:
                     ccomment = cline[8:]
                     commentp = True
-                if cline[:5] == 'Exec=' and not execp:
+                if cline[:5] == "Exec=" and not execp:
                     cexec = cline[5:]
                     execp = True
-                if cline[:9] == 'Terminal=' and not terminalp:
+                if cline[:9] == "Terminal=" and not terminalp:
                     cterminal = cline[9:]
                     terminalp = True
 
             # Get rid of these things
-            for c in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
-                cexec = cexec.replace('%' + c, '')
+            for c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                cexec = cexec.replace("%" + c, "")
             # Finally, put them into progs, but only if it has a name!
-            if cname.strip() != '':
-                progs.append((cname.strip(), ccomment.strip(), cexec.strip(),
-                              cterminal.strip()))
+            if cname.strip() != "":
+                progs.append(
+                    (cname.strip(), ccomment.strip(), cexec.strip(), cterminal.strip())
+                )
 
     return sorted(progs)
